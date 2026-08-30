76cdf3f380badd54abac4c1188dcd85769489819
abhinab25csu012 <abhinab25csu012@ncuindia.edu>
2026-08-30 17:50:46 +0530

    Add BookLoanDriver.java

---
+++ bookloandriver.java
@@ -0,0 +1,12 @@
+public class BookLoanDriver {
+    public static void main(String[] args) throws Exception {
+        BookLoan loan = new BookLoan("CH");
+        
+        System.out.println(loan.borrowbook());
+        System.out.println(loan.borrowbook());
+        System.out.println(loan.returnBook());
+        System.out.println(loan.isAvailable());
+        
+    }
+}
+ 
